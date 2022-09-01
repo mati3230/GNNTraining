28fd1ea9b2bb8a85024ed58bac200add9ae58027
Marcel Tiator <tiator.ma@gmail.com>
2022-09-01 17:16:48 +0200

    debug kfold

--- optimization/kfold_tf_client.py
+++ optimization/kfold_tf_client.py
@@ -222,6 +222,8 @@ class KFoldTFWorker(KFoldWorker):
     def load_dataset(self):
         train_folds = list(range(self.k_fold))
         test_fold = self.train_step
+        if test_fold >= self.k_fold:
+            test_fold = 0
         del train_folds[test_fold]
 
         train_data, test_data = self.load_folds(k_fold_dir=self.k_fold_dir, train_folds=train_folds,
